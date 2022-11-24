601339feba5218b9adfe172ad1a64baf8bf885b6
edsonibarra <edsonibarra97@gmail.com>
2022-11-23 21:53:34 -0600

    optimized

--- sorting/bubble_sort.py
+++ sorting/bubble_sort.py
@@ -3,12 +3,14 @@ from typing import List
 
 def bubble_sort(arr: List[int]) -> None:
     is_sorted: bool = False
+    counter: int = 0
     while not is_sorted:
         is_sorted = True
-        for i in range(len(arr) - 1):
+        for i in range(len(arr) - 1 - counter):
             if arr[i] > arr[i + 1]:
                 swap_values(i, i + 1, arr)
                 is_sorted = False
+        count += 1
 
 
 def swap_values(i, j, arr):
