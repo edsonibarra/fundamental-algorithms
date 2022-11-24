cb013ffea15d1b4979263ecfb77e4cf01da7e387
edsonibarra <edsonibarra97@gmail.com>
2022-11-23 22:01:11 -0600

    test cases with negative numbers added

--- tests/sorting/bubble_sort/test_bubble_sort.py
+++ tests/sorting/bubble_sort/test_bubble_sort.py
@@ -2,7 +2,7 @@ import pytest
 from sorting.bubble_sort import bubble_sort
 
 @pytest.mark.parametrize(
-    'arr,expected',
+    ('arr','expected'),
     (
         ([1,2,3,4,5], [1,2,3,4,5]),
         ([2,1,4,3,5], [1,2,3,4,5]),
@@ -10,6 +10,10 @@ from sorting.bubble_sort import bubble_sort
         ([1], [1]),
         ([],[]),
         ([10,4,1],[1,4,10]),
+        ([1,1,1],[1,1,1]),
+        ([-10,4,1],[-10,1,4]),
+        ([0,-1,-100],[-100, -1, 0]),
+        ([-1_000,-10_000,-1_000_000,1],[-1_000_000,-10_000,-1_000,1]),
     )
 )
 def test_bubble_sort(arr, expected):
