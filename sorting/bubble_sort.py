20e50f4d694778cb0495b779bea66308e950161c
edsonibarra <edsonibarra97@gmail.com>
2022-11-23 21:37:09 -0600

    bubble sort completed

--- sorting/bubble_sort.py
+++ sorting/bubble_sort.py
@@ -1,5 +1,14 @@
 from typing import List
 
 
-def bubble_sort(n: List[int]) -> None:
-    pass
+def bubble_sort(arr: List[int]) -> None:
+    is_sorted: bool = False
+    while not is_sorted:
+        is_sorted = True
+        for i in range(len(arr) - 1):
+            swap_values(i, i + 1, arr)
+            is_sorted = False
+
+
+def swap_values(i, j, arr):
+    arr[i], arr[j] = arr[j], arr[i]
